d8cb74a8983542f1bd6b5aaccaf6b590b44ad9e2
wujingkai111 <67622114+wujingkai111@users.noreply.github.com>
2020-10-19 03:17:34 +0800

    added end screen and buttons

---
+++ Don't Study/Assets/Scripts/GoBacktoMainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoBacktoMainMenu : MonoBehaviour
+{
+    public Animator transitionAnim;
+    public string sceneName;
+    public void GoBack()
+    {
+        StartCoroutine(LoadScene());
+
+    }
+    IEnumerator LoadScene()
+    {
+        transitionAnim.SetTrigger("End");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(sceneName);
+    }
+    
+}
