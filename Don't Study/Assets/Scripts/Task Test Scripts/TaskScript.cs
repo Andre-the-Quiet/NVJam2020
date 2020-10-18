acfd9c46bacd81669d298644bcf27775b77a93eb
Masato Lin <masatoli@usc.edu>
2020-10-18 13:04:41 -0700

    trying to work on some task stuff

--- Don't Study/Assets/Scripts/Task Test Scripts/TaskScript.cs
+++ Don't Study/Assets/Scripts/Task Test Scripts/TaskScript.cs
@@ -4,11 +4,45 @@ using UnityEngine;
 
 public class TaskScript : MonoBehaviour
 {
+    public GameObject taskPopup;
+    bool canMove;
+    int button;
     public GameObject button1;
     public GameObject button2;
     public GameObject button3;
     public GameObject button4;
-    public void Reappear()
+
+    void Start()
+    {
+        taskPopup.SetActive(false);
+        canMove = true;
+        button = 0;
+    }
+
+    //all input here
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            taskPopup.SetActive(true);
+            canMove = false;
+            taskPopup.GetComponent<TaskScript>().Reappear();
+        }
+
+    }
+
+    public void CloseTask()
+    {
+        button++;
+        if (button >= 2)
+        {
+            taskPopup.SetActive(false);
+            canMove = true;
+            button = 0;
+        }
+    }
+public void Reappear()
     {
         button1.SetActive(true);
         button2.SetActive(true);
