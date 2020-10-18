3705f03a1dad97a185ea76983bf5ceaf580a9568
Masato Lin <masatoli@usc.edu>
2020-10-17 19:56:44 -0700

    there is now working task in tast test scene

---
+++ Don't Study/Assets/Scripts/Task Test Scripts/TaskScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskScript : MonoBehaviour
+{
+    public GameObject button1;
+    public GameObject button2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Reappear()
+    {
+        button1.SetActive(true);
+        button2.SetActive(true);
+    }
+}
