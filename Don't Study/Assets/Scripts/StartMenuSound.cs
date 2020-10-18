71d30b655c90de80d9e94b94607f8ac1f26575a5
wujingkai111 <67622114+wujingkai111@users.noreply.github.com>
2020-10-18 15:59:29 +0800

    Start Menu

---
+++ Don't Study/Assets/Scripts/StartMenuSound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenuSound : MonoBehaviour
+{
+    public AudioClip HoverFX;
+    public AudioSource StartMenuSoundFX;
+    public AudioClip PressedFX;
+
+    public void Hover()
+    {
+        StartMenuSoundFX.PlayOneShot(HoverFX);
+    }
+    public void Pressed()
+    {
+        StartMenuSoundFX.PlayOneShot(PressedFX);
+    }
+}
