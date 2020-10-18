e91e7f98e0d7251689ff916ee8e4a676e6890d0e
Masato Lin <masatoli@usc.edu>
2020-10-18 13:31:29 -0700

    task code works

--- Don't Study/Assets/Scripts/Task Test Scripts/TaskScript.cs
+++ Don't Study/Assets/Scripts/Task Test Scripts/TaskScript.cs
@@ -4,18 +4,15 @@ using UnityEngine;
 
 public class TaskScript : MonoBehaviour
 {
-    public GameObject taskPopup;
-    bool canMove;
+    public GameObject player;
     int button;
     public GameObject button1;
     public GameObject button2;
-    public GameObject button3;
-    public GameObject button4;
+
 
     void Start()
     {
-        taskPopup.SetActive(false);
-        canMove = true;
+        //gameObject.SetActive(false);
         button = 0;
     }
 
@@ -23,13 +20,14 @@ public class TaskScript : MonoBehaviour
     void Update()
     {
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            taskPopup.SetActive(true);
-            canMove = false;
-            taskPopup.GetComponent<TaskScript>().Reappear();
-        }
 
+    }
+
+    public void activate()
+    {
+        gameObject.SetActive(true);
+        button = 0;
+        Reappear();
     }
 
     public void CloseTask()
@@ -37,8 +35,8 @@ public class TaskScript : MonoBehaviour
         button++;
         if (button >= 2)
         {
-            taskPopup.SetActive(false);
-            canMove = true;
+            gameObject.SetActive(false);
+            player.GetComponent<PlayerControlllerTask>().move();
             button = 0;
         }
     }
@@ -46,7 +44,6 @@ public void Reappear()
     {
         button1.SetActive(true);
         button2.SetActive(true);
-        button3.SetActive(true);
-        button4.SetActive(true);
+
     }
 }
