46ea85d16a2a7a5718414bf2ab2bd1c02778922c
Masato Lin <masatoli@usc.edu>
2020-10-18 04:56:10 -0700

    working on the popups

--- Don't Study/Assets/Scripts/Task Test Scripts/TaskScript.cs
+++ Don't Study/Assets/Scripts/Task Test Scripts/TaskScript.cs
@@ -6,9 +6,13 @@ public class TaskScript : MonoBehaviour
 {
     public GameObject button1;
     public GameObject button2;
+    public GameObject button3;
+    public GameObject button4;
     public void Reappear()
     {
         button1.SetActive(true);
         button2.SetActive(true);
+        button3.SetActive(true);
+        button4.SetActive(true);
     }
 }
